ee0c619f4f46b2e51b29eed4e7bd0a6e7ee6217e
Rajat Jindal <rajatjindal83@gmail.com>
2021-06-02 11:12:44 +0530

    update

--- store/userinfo.js
+++ store/userinfo.js
@@ -1,31 +1,51 @@
 export const state = () => ({
+    processing: [],
     userInfo: []
 })
 
 export const getters = {
     getUserInfo: state => id => {
        return state.userInfo.find(item => id == item.id)
-    }
+    },
+    processing: state => id => {
+        return state.processing.find(item => id == item.id)
+    },
+   
 }
 
 export const mutations = {
     add(state, value) {
         state.userInfo.push(value)
-        console.log(state.userInfo)
     },
+    startProcessing(state, value) {
+        state.processing.push(value)
+    },
+    finishProcessing (state, id) {
+        state.processing.splice(state.processing.findIndex(item => id == item.id), 1)
+    }
 }
 
 export const actions = {
     async fetchUserInfo({ commit, getters}, id) {
-        const item = getters.getUserInfo(id)
-        if (item) {
+        if (!id || id == "") {
             return;
         }
         
+        const item = getters.processing(id)
+        if (item) {
+            return;
+        }
+
+        //to prevent duplicate calls to backend api
+        commit('startProcessing', {id: id})
+
         var res = await this.$userinfo({
             method: "get",
             url: "/userinfo?id=" + id,
         })
+
+        console.log(res.data)
         commit('add', res.data);
+        commit('finishProcessing', id)
     }
 }
