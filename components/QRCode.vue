1a0077c9b9912b2ace9efd8828c16855687f906d
Rajat Jindal <rajatjindal83@gmail.com>
2021-06-14 07:32:14 +0530

    update

--- components/QRCode.vue
+++ components/QRCode.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="hidden lg:block sticky top-20 left-4">
-    <img class="ml-2 w-48 h-48" src="/qrcode.png" />
+    <img
+      class="ml-2 w-48 h-48"
+      :src="
+        'https://webhook.translatethread.com/qrcode?link=https://e.translatethread.com/thread/' +
+          $route.params.id
+      "
+    />
     <p class="ml-4 text-xs text-gray-400">scan qr code to open on mobile</p>
   </div>
 </template>
