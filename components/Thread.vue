6f79e2fe41f4d305050604cb521ec3ff20361d5e
Daniel Boros <boros.daniel@protonmail.com>
2021-11-25 12:10:18 +0100

    Moved request to asyncData which is executed before load
    Signed-off-by: Daniel Boros <boros.daniel@protonmail.com>

--- components/Thread.vue
+++ components/Thread.vue
@@ -12,130 +12,17 @@
 <script>
 import Tweet from "~/components/Tweet.vue";
 import Summary from "~/components/Summary.vue";
-import { mapGetters, mapActions } from "vuex";
 
 export default {
   components: {
     Tweet,
     Summary,
   },
-  data() {
-    return {
-      threadObject: {},
-      title: "Translated Thread",
-      authorHandle: "",
-    };
-  },
-  async fetch() {
-    const res = await this.fetchTranslatedThread(this.$route.params.id);
-    this.threadObject = res.data;
-    this.title =
-      this.threadObject.Thread[0].Author +
-      " @" +
-      this.threadObject.Thread[0].AuthorHandle;
-
-    this.authorHandle = this.threadObject.Thread[0].AuthorHandle;
-  },
-  head() {
-    return {
-      title: this.title,
-      link: [
-        {
-          rel: "icon",
-          href: "/favicon.svg",
-          type: "image/x-icon",
-        },
-      ],
-      meta: [
-        {
-          charset: "utf-8",
-        },
-        {
-          name: "content-type",
-          "http-equiv": "Content-type",
-          content: "text/html; charset=UTF-8",
-        },
-        {
-          name: "viewport",
-          content: "width=device-width, initial-scale=1",
-        },
-        {
-          hid: "og:title",
-          name: "og:title",
-          content: `successfully translated thread by @${this.authorHandle}`,
-        },
-        {
-          hid: "og:image",
-          name: "og:image",
-          content: `https://api.translatethread.com/${this.$route.params.id}.jpeg`,
-        },
-        {
-          hid: "og:image:alt",
-          name: "og:image:alt",
-          content: "Translate Thread",
-        },
-        {
-          hid: "og:url",
-          name: "og:url",
-          content: `https://api.translatethread.com/thread/${this.$route.params.id}`,
-        },
-        {
-          hid: "og:description",
-          name: "og:description",
-          content: `translated version of thread by @${this.authorHandle} is available now.`,
-        },
-        {
-          hid: "og:image:width",
-          name: "og:image:width",
-          content: "1200",
-        },
-        {
-          hid: "og:image:height",
-          name: "og:image:height",
-          content: "628",
-        },
-        {
-          hid: "og:image:type",
-          name: "og:image:type",
-          content: "image/png",
-        },
-        {
-          hid: "twitter:card",
-          name: "twitter:card",
-          content: "summary_large_image",
-        },
-        {
-          hid: "twitter:site",
-          name: "twitter:site",
-          content: "@translatethread",
-        },
-        {
-          hid: "twitter:creator",
-          name: "twitter:creator",
-          content: "@translatethread",
-        },
-        {
-          hid: "twitter:title",
-          name: "twitter:title",
-          content: `successfully translated thread by @${this.authorHandle}`,
-        },
-        {
-          hid: "twitter:description",
-          name: "twitter:description",
-          content: `translated version of thread by @${this.authorHandle} is available now.`,
-        },
-        {
-          hid: "twitter:image",
-          name: "twitter:image",
-          content: `https://api.translatethread.com/${this.$route.params.id}.jpeg`,
-        },
-        {
-          hid: "twitter:domain",
-          name: "twitter:domain",
-          content: `https://api.translatethread.com/thread/${this.$route.params.id}`,
-        },
-      ],
-    };
+  props: {
+    threadObject: {
+      type: Object,
+      required: true
+    }
   },
   mounted: function() {
     // Ensure the browser navigates to the correct place in the page after
@@ -144,15 +31,11 @@ export default {
     this.$nextTick(() => this.scrollFix('[id="' + id + '"]'));
   },
   methods: {
-    ...mapActions({ fetchTranslatedThread: "thread/fetchTranslatedThread" }),
     scrollFix: function(hash) {
       if (hash && document.querySelector(hash)) {
         location.href = hash;
       }
     },
-  },
-  computed: {
-    ...mapGetters("thread", ["getTranslatedThread"]),
   },
 };
 </script>
--- pages/thread/_id.vue
+++ pages/thread/_id.vue
@@ -7,7 +7,7 @@
       <div class="col-span-2 hidden lg:block">
         <QRCode />
       </div>
-      <thread class="lg:col-start-3 lg:col-span-7 col-span-12 w-full" />
+      <thread :thread-object="threadObject" class="lg:col-start-3 lg:col-span-7 col-span-12 w-full" />
     </div>
     <Footer />
   </div>
@@ -25,6 +25,119 @@ export default {
     QRCode,
     Thread,
     Footer,
+  },
+  async asyncData({ store, route }) {
+    const { data } = await store.dispatch('thread/fetchTranslatedThread', route.params.id);
+    return { threadObject: data }
+  },
+  computed: {
+    title() {
+      return `${this.threadObject.Thread[0].Author} @${this.threadObject.Thread[0].AuthorHandle}`
+    },
+    authorHandle() {
+      return this.threadObject.Thread[0].AuthorHandle
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+      link: [
+        {
+          rel: "icon",
+          href: "/favicon.svg",
+          type: "image/x-icon",
+        },
+      ],
+      meta: [
+        {
+          charset: "utf-8",
+        },
+        {
+          name: "content-type",
+          "http-equiv": "Content-type",
+          content: "text/html; charset=UTF-8",
+        },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          hid: "og:title",
+          name: "og:title",
+          content: `successfully translated thread by @${this.authorHandle}`,
+        },
+        {
+          hid: "og:image",
+          name: "og:image",
+          content: `https://api.translatethread.com/${this.$route.params.id}.jpeg`,
+        },
+        {
+          hid: "og:image:alt",
+          name: "og:image:alt",
+          content: "Translate Thread",
+        },
+        {
+          hid: "og:url",
+          name: "og:url",
+          content: `https://api.translatethread.com/thread/${this.$route.params.id}`,
+        },
+        {
+          hid: "og:description",
+          name: "og:description",
+          content: `translated version of thread by @${this.authorHandle} is available now.`,
+        },
+        {
+          hid: "og:image:width",
+          name: "og:image:width",
+          content: "1200",
+        },
+        {
+          hid: "og:image:height",
+          name: "og:image:height",
+          content: "628",
+        },
+        {
+          hid: "og:image:type",
+          name: "og:image:type",
+          content: "image/png",
+        },
+        {
+          hid: "twitter:card",
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+        {
+          hid: "twitter:site",
+          name: "twitter:site",
+          content: "@translatethread",
+        },
+        {
+          hid: "twitter:creator",
+          name: "twitter:creator",
+          content: "@translatethread",
+        },
+        {
+          hid: "twitter:title",
+          name: "twitter:title",
+          content: `successfully translated thread by @${this.authorHandle}`,
+        },
+        {
+          hid: "twitter:description",
+          name: "twitter:description",
+          content: `translated version of thread by @${this.authorHandle} is available now.`,
+        },
+        {
+          hid: "twitter:image",
+          name: "twitter:image",
+          content: `https://api.translatethread.com/${this.$route.params.id}.jpeg`,
+        },
+        {
+          hid: "twitter:domain",
+          name: "twitter:domain",
+          content: `https://api.translatethread.com/thread/${this.$route.params.id}`,
+        },
+      ],
+    };
   },
 };
 </script>
